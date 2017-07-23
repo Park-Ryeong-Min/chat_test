6a3b08831a725847a01d10365fbd06bddcc79e77
Ryeong-Min <somuch0321@gmail.com>
2017-07-23 16:43:37 +0900

    simple connection module

---
+++ chat_modules/chat_on.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Myown on 2017-07-23.
+ */
+
+var http = require('http');
+var io = require('socket.io')(http);
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function () {
+        console.log('disconnect');
+    })
+});
+
+module.exports = io;
---
+++ public/javascripts/socket_conn.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Myown on 2017-07-23.
+ */
+var socket = io();
