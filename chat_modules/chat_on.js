5becc9171b59986c8f8aa00fdddd9df3607bf279
Ryeong-Min <somuch0321@gmail.com>
2017-07-23 22:24:35 +0900

    socket.join test

--- chat_modules/chat_on.js
+++ chat_modules/chat_on.js
@@ -7,10 +7,18 @@ var io = require('socket.io')(http);
 
 io.on('connection', function (socket) {
     console.log('a user connected');
+    socket.emit('join',{data:"Welcome"});
     socket.on('disconnect', function () {
         console.log('disconnect');
     });
-    socket.emit('join',{data:"Welcome"});
+    socket.on('join_room', function (data) {
+       socket.join(data.room_id);
+       var msg = data.room_id + " has joined";
+       socket.emit('notice_join_room', {data:msg});
+    });
+    socket.on('send_message', function (data) {
+        socket.in(data.room_id).emit('send_message', data.message);
+    });
 });
 
 module.exports = io;
